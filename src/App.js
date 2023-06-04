a5363ca28afe4213e97dbaf465f3e3572b1d7c29
Dagna Schmidt <dagdagi889@gmail.com>
2023-06-04 20:05:46 +0200

    part5 5.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState('');
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then(blogs => setBlogs(blogs));
+  }, []);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const user = await loginService.login({username, password});
+      console.log(user);
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    }
+    catch (exception) {
+      setErrorMessage('Wrong credentials');
+      setTimeout(() => {setErrorMessage(null)}, 5000);
+    }
+  }
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      <div>{errorMessage}</div>
+        {user ?
+          <>
+            <h2>blogs</h2>
+            <h5> logged in</h5>
+            {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
+          </>
+        :
+          <>
+            <h2>login</h2>
+            <form onSubmit={handleLogin}>
+              <div>
+                <label>username</label>
+                <input
+                  type='text'
+                  value={username}
+                  name='username'
+                  onChange={({target}) => setUsername(target.value)}
+                />
+              </div>
+              <div>
+                <label>password</label>
+                <input
+                  type='text'
+                  value={password}
+                  name='password'
+                  onChange={({target}) => setPassword(target.value)}
+                />
+              </div>
+              <button>login</button>
+            </form>
+          </>
+        }
     </div>
   )
-}
+};
 
-export default App
+export default App;
